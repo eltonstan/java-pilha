e8fcedca717e3b84e0dcfef922d28b7c8edd9d17
Elton Standerski <eltonstand@gmail.com>
2020-08-08 11:12:02 -0300

    Aula 02: try catch

--- src/Pilha.java
+++ src/Pilha.java
@@ -1,15 +1,9 @@
 
-public class Fluxo {
+public class Pilha {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        try{
-            metodo1();
-        } catch(ArithmeticException | NullPointerException ex) {
-            String msg = ex.getMessage();
-            System.out.println("Exception " + msg);
-            ex.printStackTrace();
-        }
+        metodo1();
         System.out.println("Fim do main");
     }
 
@@ -23,8 +17,6 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-            Conta c = null;
-            c.deposita();
         }
         System.out.println("Fim do metodo2");        
     }
